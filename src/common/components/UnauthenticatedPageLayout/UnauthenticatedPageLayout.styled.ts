f7c07472c201973f44fce6db006d2d2404a58a07
alimirpp <amirkharaz@peerplays.com>
2022-01-07 15:53:08 +0330

    styling instruction for components

---
+++ src/common/components/UnauthenticatedPageLayout/UnauthenticatedPageLayout.styled.ts
@@ -0,0 +1,6 @@
+import { Flex, styled } from '../../../../ui/src'
+
+export const UnauthenticatedPageLayoutContainer = styled(Flex)`
+  flex: auto;
+  flex-direction: row;
+`
---
+++ ui/src/index.ts
@@ -0,0 +1,2 @@
+export { default as styled, css } from 'styled-components'
+export { Box, Flex } from 'reflexbox/styled-components'
