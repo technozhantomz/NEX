4ace080b5499b1c87d62114ac809a0998d2e77a0
alimirpp <amirkharaz@peerplays.com>
2022-01-10 12:51:25 +0330

    eslint config

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,18 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import { PeerplaysApiProvider } from '../modules/peerplaysApi'
-import { ConnectionManager } from '../common/components/ConnectionManager'
+import "../styles/globals.css";
 
+import type { AppProps } from "next/app";
 
+import { ConnectionManager } from "../common/components/ConnectionManager";
+import { PeerplaysApiProvider } from "../modules/peerplaysApi";
 
-function App({ Component, pageProps }: AppProps) {
-    return (
-      <PeerplaysApiProvider>
-        <ConnectionManager>
-          <Component {...pageProps} />
-        </ConnectionManager>
-      </PeerplaysApiProvider>
-    )
-  }
+function App({ Component, pageProps }: AppProps): JSX.Element {
+  return (
+    <PeerplaysApiProvider>
+      <ConnectionManager>
+        <Component {...pageProps} />
+      </ConnectionManager>
+    </PeerplaysApiProvider>
+  );
+}
 
-export default App
+export default App;
