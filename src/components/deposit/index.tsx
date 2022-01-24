2c173b75956b14af2c467e784e2fc7157764e3de
blockchain-jsdev <>
2022-01-24 13:12:45 -0400

    responsive layout

---
+++ src/components/deposit/index.tsx
@@ -0,0 +1,12 @@
+import { Button, Card, Col, Row } from "antd";
+import Styles from '../../styles/depositComponent.module.scss'
+
+const Deposit = (): JSX.Element => {
+  return (
+    <div className={Styles.depositContainer}>
+        <Button className={Styles.logInBtn}>Log in & Generate Bitcoin Address</Button>
+    </div>
+  );
+};
+
+export default Deposit;
--- src/pages/deposit/index.tsx
+++ src/pages/deposit/index.tsx
@@ -1,50 +1,26 @@
 import type { NextPage } from "next";
 import { Button, Card, Col, Row } from "antd";
 import Layout from "../../components/layout";
+import Deposit from "../../components/deposit"
+import Styles from '../../styles/depositPage.module.scss';
 
-import Styles from '../../styles/deposit.module.scss';
+const DepositPage: NextPage = () => {
 
-const Deposit: NextPage = () => {
   return (
     <Layout title="Deposit">
-      <Row>
-        <Col span={12} className={Styles.header_container}>
-          <Card className={Styles.content_header}>
-
-            <div>
-              <Button className={Styles.deposit_btn}>Deposit</Button>
-              <Button className={Styles.deposit_btn}>Withdraw</Button>
-              <Button className={Styles.deposit_btn}>Swap</Button>
-              <Button className={Styles.deposit_btn}>Market</Button>
-
-            </div>
-            {/* <Row>
-              <Col span={6}>
-                <Button className={Styles.deposit_btn}>Deposit</Button>
-              </Col>
-              <Col span={6}>
-                <Button className={Styles.deposit_btn}>Withdraw</Button>
-              </Col>
-              <Col span={6}>
-                <Button className={Styles.deposit_btn}>Swap</Button>
-              </Col>
-              <Col span={6}>
-                <Button className={Styles.deposit_btn}>Market</Button>
-              </Col>
-            </Row> */}
-          </Card>
-        </Col>
-      </Row>
-
-      <Row>
-        <Col span={12} className={Styles.header_container}>
-          <Card className={Styles.content_body}>
-
-          </Card>
-        </Col>
-      </Row>
+      <div className={Styles.headerContainer}>
+        <div className={Styles.headerContainerItem}>
+          <Button className={Styles.btn}>Deposit</Button>
+          <Button className={Styles.btn}>Wtihdraw</Button>
+          <Button className={Styles.btn}>Swap</Button>
+          <Button className={Styles.btn}>Market</Button>
+        </div>
+      </div>
+      <div className={Styles.bodyContainer}>
+        <Deposit />
+      </div>
     </Layout>
   );
 };
 
-export default Deposit;
+export default DepositPage;
