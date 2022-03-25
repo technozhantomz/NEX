ceae6298d3d2e5be7a0eeae5aeefb34cd2522e55
blockchain-jsdev <>
2022-03-25 05:59:31 -0300

    #94 init setup

---
+++ src/modules/Voting/components/WitnessTab/WitnessTab.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import * as Styled from "./WitnessTab.styled";
+
+export const WitnessTab = (): JSX.Element => {
+  return (
+    <Styled.WitnessTabCard>
+      <Styled.Text>Vote for Witnesses</Styled.Text>
+    </Styled.WitnessTabCard>
+  );
+};
