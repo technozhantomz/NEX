e3db0626e1cce3c4bd0f3a70807a8fb602ce031e
blockchain-jsdev <>
2022-03-28 14:11:19 -0300

    UI first action part

--- src/modules/Voting/components/WitnessTab/WitnessTab.tsx
+++ src/modules/Voting/components/WitnessTab/WitnessTab.tsx
@@ -13,46 +13,42 @@ export const WitnessTab = (): JSX.Element => {
       <Styled.Text>Vote for Witnesses</Styled.Text>
 
       <Divider orientation="left">Responsive</Divider>
-      <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 12 }}>
+      {/* <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 12 }}>
+
         <Col xs={24} sm={4} md={6} lg={6}>
           <div style={style}>Vote for Witnesses</div>
         </Col>
-        <Col xs={24} sm={4} md={6} lg={12} >
-          {/* <div style={style}>search</div> */}
+        <Col xs={24} sm={4} md={6} lg={12} > */}
           <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 12 }} style={style}>
-            <Col xs={24} sm={4} md={6} lg={6}>
+        <Col xs={24} sm={4} md={6} lg={6}>
+        {/* <Col xs={24} sm={4} md={6} lg={6}> */}
+          <div style={style}>Vote for Witnesses</div>
+        {/* </Col> */}
+        </Col>
+
+        <Col xs={24} lg={18}>
+            <Col xs={{ span: 24, order: 1 }}  md={{ span: 6, order: 1 }}>
+              
               <div style={style2}>Search accounts</div>
             </Col>
-            <Col xs={24} sm={4} md={6} lg={16}>
+
+            <Col xs={{ span: 24, order: 2 }}  md={{ span: 4, order: 2 }}>
               <div style={style2}>Add</div>
             </Col>
 
-            <Col xs={24} sm={4} md={6} lg={6}>
+            <Col xs={{ span: 24, order: 4 }}  md={{ span: 6, order: 3 }}>
               <div style={style2}>reset</div>
             </Col>
-            <Col xs={24} sm={4} md={6} lg={16}>
+
+            <Col xs={{ span: 24, order: 3 }}  md={{ span: 16, order: 4 }}>
               <div style={style2}>publish</div>
             </Col>
-            </Row>
         </Col>
-        {/* <Col xs={24} sm={4} md={6} lg={4}>
-          <div style={style}>Buttons</div>
-          <div style={style}>Buttons</div>
-        </Col> */}
-       
+            </Row>
+        {/* </Col>
+        
+      </Row> */}
 
-        {/* <Row>
-          <Col xs={2} sm={4} md={6} lg={8} xl={10}>
-            Col
-          </Col>
-          <Col xs={20} sm={16} md={12} lg={8} xl={4}>
-            Col
-          </Col>
-          <Col xs={2} sm={4} md={6} lg={8} xl={10}>
-            Col
-          </Col>
-        </Row>, */}
-      </Row>
     </Styled.WitnessTabCard>
   );
 };
