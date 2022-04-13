fdf5f04ce500708833c8a408d443326c5694d939
Evan Lynch <elynch@peerplays.com>
2022-04-13 12:20:04 -0400

    #136 init get download link working

--- src/modules/Signup/components/InfoBar/InfoBar.tsx
+++ src/modules/Signup/components/InfoBar/InfoBar.tsx
@@ -1,18 +1,27 @@
-import Link from "next/link";
 import React from "react";
 
 import { InfoCircleOutlined } from "../../../../ui/src";
+import { useSignUpForm } from "../SignUpForm/hooks";
 
 import * as Styled from "./InfoBar.styled";
+import { getRecoveryPassword } from "./hooks/getRecoveryPassword";
 
 export const InfoBar: React.FC = () => {
+  const { signUpForm } = useSignUpForm();
   return (
     <Styled.InfoBar>
       <InfoCircleOutlined />
       <Styled.InfoBarText>
         <p>
-          <span>Keep your password safe to avoid losing any funds.</span>
-          <Link href={"/"}>Download Recovery password file here</Link>
+          <span>Keep your password safe to avoid losing any funds. </span>
+          <a
+            href="#"
+            onClick={() => {
+              getRecoveryPassword(signUpForm.getFieldValue("password"));
+            }}
+          >
+            Download Recovery password file here
+          </a>
         </p>
       </Styled.InfoBarText>
     </Styled.InfoBar>
