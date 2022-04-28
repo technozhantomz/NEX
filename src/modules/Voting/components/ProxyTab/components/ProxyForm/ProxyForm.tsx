334bbf0768c0743bec0ff3811fab1a4882d1826b
Evan Lynch <elynch@peerplays.com>
2022-04-28 08:47:54 -0400

    updates

--- src/modules/Voting/components/ProxyTab/components/ProxyForm/ProxyForm.tsx
+++ src/modules/Voting/components/ProxyTab/components/ProxyForm/ProxyForm.tsx
@@ -1,20 +1,50 @@
 import { PasswordModal } from "../../../../../../common/components";
-import { Form, RedoOutlined } from "../../../../../../ui/src";
+import { Form, Input, RedoOutlined } from "../../../../../../ui/src";
 
 import * as Styled from "./ProxyForm.styled";
+import { useProxyForm } from "./hooks";
 
 export const ProxyForm = (): JSX.Element => {
+  const {
+    proxyForm,
+    formValidator,
+    isPasswordModalVisible,
+    submittingPassword,
+    confirm,
+    onFormFinish,
+    handleSearch,
+    handlePasswordModalCancel,
+  } = useProxyForm();
+
   return (
-    <Form.Provider>
-      <Styled.ProxyForm>
-        <Form.Item>
-          <Styled.ProxyFormSearch />
-        </Form.Item>
-        <Form.Item>
-          <Styled.ProxyFormButton type="primary">Add</Styled.ProxyFormButton>
+    <Form.Provider onFormFinish={onFormFinish}>
+      <Styled.ProxyForm
+        form={proxyForm}
+        name="proxyForm"
+        onFinish={confirm}
+        size="large"
+      >
+        {/* <Form.Item>
+          <Input />
+        </Form.Item> */}
+        <Form.Item
+          name="proxyUsername"
+          rules={formValidator.proxyUsername}
+          validateFirst={true}
+          validateTrigger="onBlur"
+        >
+          <Styled.ProxyFormSearch
+            placeholder="Search Accounts"
+            onSearch={handleSearch}
+          />
         </Form.Item>
         <Form.Item>
           <Styled.ProxyFormButton type="primary">
+            Add
+          </Styled.ProxyFormButton>
+        </Form.Item>
+        <Form.Item>
+          <Styled.ProxyFormButton type="primary" htmlType="submit">
             Publish changes
           </Styled.ProxyFormButton>
         </Form.Item>
@@ -25,11 +55,9 @@ export const ProxyForm = (): JSX.Element => {
         </Form.Item>
       </Styled.ProxyForm>
       <PasswordModal
-        visible={false}
-        onCancel={function (): void {
-          throw new Error("Function not implemented.");
-        }}
-        submitting={false}
+        visible={isPasswordModalVisible}
+        onCancel={handlePasswordModalCancel}
+        submitting={submittingPassword}
       />
     </Form.Provider>
   );
