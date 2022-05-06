ebad23800a1f390e69e3aca47b4e984d3109d000
Alexis Tanaka <legendlancer0314@gmail.com>
2022-05-06 04:59:10 -0700

    css-fix

--- src/modules/Wallet/components/AssetTitle/AssetTitle.styled.tsx
+++ src/modules/Wallet/components/AssetTitle/AssetTitle.styled.tsx
@@ -1,8 +1,18 @@
 import { styled, List as UiList } from "../../../../ui/src";
 import { colors } from "../../../../ui/src/colors";
 
-export const AssetTitle = styled(UiList.Item.Meta)`
+export const AssetTitle = styled(UiList.Item.Meta)`{
+  align-items: center;
+  .ant-list-item-meta-avatar{
+    margin-right: 10px;
+  }
+  .ant-list-item-meta-content {
+    .ant-list-item-meta-title {
+      margin-bottom: 0;
+    }
+  }
   .asset-symbol {
     color: ${colors.textColorSecondary};
   }
+}
 `;
