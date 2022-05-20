bb92c074aea6a6f89f3387d52cf3d252d9b24668
Alexis Tanaka <legendlancer0314@gmail.com>
2022-05-19 23:41:31 -0700

    #175 blockchain page-fee tab-fee tags-breaks into two lines

--- src/modules/Blockchain/components/FeesTab/FeesTab.styled.tsx
+++ src/modules/Blockchain/components/FeesTab/FeesTab.styled.tsx
@@ -20,7 +20,6 @@ export const Section = styled.section`
 `;
 
 export const FeesTable = styled(UiTable)`
-  max-width: 592px;
   .ant-table-thead > tr > th {
     color: ${colors.textColorSecondary};
     background: ${colors.white};
@@ -36,6 +35,18 @@ export const FeesTable = styled(UiTable)`
     vertical-align: baseline;
     div:not(:last-child) {
       margin-bottom: 15px;
+    }
+    ${breakpoint.sm} {
+      padding: 16px 0px;
+      span {
+        padding: 5px 16px;
+      }
+    }
+    ${breakpoint.md} {
+      padding: 16px 16px;
+      span {
+        padding: 5px 21px;
+      }
     }
   }
   .ant-tag {
@@ -74,8 +85,8 @@ export const FeeItemContent = styled.div`
     }
     .fee-info-value {
       font-weight: 500;
-      .ant-tag {
-        padding: 5px 15px;
+      span {
+        padding: 5px 5px;
         background: ${colors.assetTag};
         border: none;
         color: ${colors.textColor};
