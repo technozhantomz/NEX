6c00cbc722647b62ff020e8cadead25870e9fe83
alimirpp <amirkharaz@peerplays.com>
2022-07-04 13:54:06 +0430

    #254 adding not available for asset info

--- src/modules/Blockchain/components/AssetsTab/AssetsTab.tsx
+++ src/modules/Blockchain/components/AssetsTab/AssetsTab.tsx
@@ -88,16 +88,15 @@ export const AssetsTab = (): JSX.Element => {
                     {AssetsColumns[5].title()}
                   </span>
                   <span className="asset-info-value">
-                    <Tooltip
-                      placement="top"
-                      title={
-                        item.info == ""
-                          ? `asset with precision ${item.precision}`
-                          : item.info
-                      }
-                    >
-                      <InfoCircleOutlined />
-                    </Tooltip>
+                    {!item.info || item.info === "" ? (
+                      <span>
+                        {counterpart.translate(`field.labels.not_available`)}
+                      </span>
+                    ) : (
+                      <Tooltip placement="top" title={item.info}>
+                        <InfoCircleOutlined />
+                      </Tooltip>
+                    )}
                   </span>
                 </div>
               </Styled.AssetItemContent>
