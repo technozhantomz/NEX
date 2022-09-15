3d2878c906e7eaeef8c781e058effb5a69dc9333
Evan Lynch <elynch@peerplays.com>
2022-09-15 10:52:21 -0400

    update to pagination

--- src/modules/Blockchain/components/FeesTab/FeesTab.tsx
+++ src/modules/Blockchain/components/FeesTab/FeesTab.tsx
@@ -1,6 +1,6 @@
 import { SearchTableInput } from "ant-table-extensions";
 import counterpart from "counterpart";
-import { useRef } from "react";
+import { CSSProperties, ReactNode, useRef } from "react";
 import { CSVLink } from "react-csv";
 import ReactToPrint from "react-to-print";
 
@@ -62,9 +62,6 @@ export const FeesTab = (): JSX.Element => {
           <List
             itemLayout="vertical"
             dataSource={searchDataSource}
-            pagination={{
-              pageSize: 10,
-            }}
             loading={loading}
             renderItem={(item: FeesTableRow) => (
               <Styled.FeeListItem key={item.operation}>
@@ -132,6 +129,42 @@ export const FeesTab = (): JSX.Element => {
             dataSource={searchDataSource}
             columns={FeesColumns}
             loading={loading}
+            pagination={
+              !loading
+                ? {
+                    showSizeChanger: false,
+                    size: "small",
+                    pageSize: 15,
+                    showLessItems: true,
+                    itemRender: (
+                      _page: number,
+                      type:
+                        | "page"
+                        | "prev"
+                        | "next"
+                        | "jump-prev"
+                        | "jump-next",
+                      element: ReactNode
+                    ) => {
+                      if (type === "prev") {
+                        return (
+                          <a style={{ marginRight: "8px" } as CSSProperties}>
+                            {counterpart.translate(`buttons.previous`)}
+                          </a>
+                        );
+                      }
+                      if (type === "next") {
+                        return (
+                          <a style={{ marginLeft: "8px" } as CSSProperties}>
+                            {counterpart.translate(`buttons.next`)}
+                          </a>
+                        );
+                      }
+                      return element;
+                    },
+                  }
+                : false
+            }
           />
         )}
         <Styled.PrintTable ref={componentRef}>
