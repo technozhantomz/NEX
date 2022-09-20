840eb18b0d3533b340c616850a7f2d1c4bdd7527
Evan Lynch <elynch@peerplays.com>
2022-09-20 11:22:12 -0400

    Blockchain Tab

--- src/modules/Blockchain/components/BlockchainTab/BlockchainTab.styled.tsx
+++ src/modules/Blockchain/components/BlockchainTab/BlockchainTab.styled.tsx
@@ -1,22 +1,48 @@
 import {
   styled,
+  ListItem as UiListItem,
+  PrintTable as UiPrintTable,
   StatsCardsDeck as UIStatsCardsDeck,
-  TableSearch as UiTableSearch,
+  // TableSearch as UiTableSearch,
 } from "../../../../ui/src";
-import { breakpoint } from "../../../../ui/src/breakpoints";
+// import { breakpoint } from "../../../../ui/src/breakpoints";
+import {
+  BlockchainDownloadLinks,
+  BlockchainHeader,
+  BlockchainHeaderBar,
+  BlockchainItemContent,
+  BlockchainTable,
+  BlockchainTabWrapper,
+} from "../../common";
 
-export const BlockTabWrapper = styled.div`
-  margin: 0 15px;
-  ${breakpoint.sm} {
-    margin: 0 25px;
-  }
-`;
+export const BlockTabWrapper = styled(BlockchainTabWrapper)``;
 
 export const StatsCardsDeck = styled(UIStatsCardsDeck)``;
 
-export const BlockSearch = styled(UiTableSearch)`
-  margin-bottom: 25px;
-  ${breakpoint.sm} {
-    margin-bottom: 35px;
+// export const BlockSearch = styled(UiTableSearch)`
+//   margin-bottom: 25px;
+//   ${breakpoint.sm} {
+//     margin-bottom: 35px;
+//   }
+// `;
+
+export const BlockHeaderBar = styled(BlockchainHeaderBar)``;
+
+export const BlockHeader = styled(BlockchainHeader)``;
+
+export const BlockTable = styled(BlockchainTable)`
+  .ant-table-thead > tr.ant-table-row-hover:not(.ant-table-expanded-row) > td,
+  .ant-table-tbody > tr.ant-table-row-hover:not(.ant-table-expanded-row) > td,
+  .ant-table-thead > tr:hover:not(.ant-table-expanded-row) > td,
+  .ant-table-tbody > tr:hover:not(.ant-table-expanded-row) > td {
+    cursor: pointer;
   }
 `;
+
+export const BlockListItem = styled(UiListItem)``;
+
+export const BlockItemContent = styled(BlockchainItemContent)``;
+
+export const DownloadLinks = styled(BlockchainDownloadLinks)``;
+
+export const PrintTable = styled(UiPrintTable)``;
--- src/modules/Blockchain/components/BlockchainTab/BlockchainTab.tsx
+++ src/modules/Blockchain/components/BlockchainTab/BlockchainTab.tsx
@@ -1,75 +1,202 @@
 import { ParsedUrlQuery } from "querystring";
 
+import { SearchTableInput } from "ant-table-extensions";
 import counterpart from "counterpart";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { CSSProperties, ReactNode, useRef } from "react";
+import { CSVLink } from "react-csv";
+import ReactToPrint from "react-to-print";
 
+import { useViewportContext } from "../../../../common/providers";
+import {
+  DownloadOutlined,
+  InfoCircleOutlined,
+  List,
+  SearchOutlined,
+} from "../../../../ui/src";
 import { StatsCard } from "../../common";
 
 import * as Styled from "./BlockchainTab.styled";
-import { BlockTable } from "./components";
+import { BlockPrintTable } from "./components";
 import { useBlockchainTab } from "./hooks";
 
 type Props = {
   routerQuery: ParsedUrlQuery;
 };
 export const BlockchainTab = ({ routerQuery }: Props): JSX.Element => {
-  const { loading, blockchainData, searchValue, searchResult, handleSearch } =
-    useBlockchainTab(routerQuery);
+  const {
+    loading,
+    blockColumns,
+    blockchainTableRows,
+    blockchainStats,
+    currentBlock,
+    lastIrreversibleBlock,
+    avgTime,
+    supply,
+    searchDataSource,
+    setSearchDataSource,
+  } = useBlockchainTab(routerQuery);
+  const router = useRouter();
+  const { sm } = useViewportContext();
+  const componentRef = useRef();
+
   return (
     <Styled.BlockTabWrapper>
       <Styled.StatsCardsDeck>
         <StatsCard
-          noData={blockchainData.currentBlock === 0}
+          noData={currentBlock === 0}
           title={counterpart.translate(`pages.blocks.blockchain.current_block`)}
-          data={`${blockchainData.currentBlock}`}
-          statsData={blockchainData.stats.blocks}
+          data={`${currentBlock}`}
+          statsData={blockchainStats.currentBlock}
         />
         <StatsCard
-          noData={blockchainData.supply.amount === 0}
-          title={counterpart.translate(`pages.blocks.blockchain.supply`, {
-            symbol: blockchainData.supply.symbol,
-          })}
-          data={`${blockchainData.supply.amount}`}
-          statsData={blockchainData.stats.supply}
-        />
-        <StatsCard
-          noData={blockchainData.activeWitnesses.length === 0}
+          noData={lastIrreversibleBlock === ""}
           title={counterpart.translate(
-            `pages.blocks.blockchain.active_witness`
+            `pages.blocks.blockchain.last_irreversible_block`
           )}
-          data={`${blockchainData.activeWitnesses.length}`}
-          statsData={blockchainData.stats.witnesses}
+          data={`${lastIrreversibleBlock}`}
+          statsData={blockchainStats.lastIrreversible}
         />
         <StatsCard
           isTimeCard={true}
-          noData={blockchainData.avgTime === 0}
+          noData={avgTime === 0}
           title={counterpart.translate(
             `pages.blocks.blockchain.confirmation_time`
           )}
-          data={`${blockchainData.avgTime}`}
-          statsData={blockchainData.stats.times}
+          data={`${avgTime}`}
+          statsData={blockchainStats.avgTime}
+        />
+        <StatsCard
+          isRewardCard
+          noData={supply.amount === 0}
+          title={counterpart.translate(`pages.blocks.blockchain.supply`, {
+            symbol: supply.symbol,
+          })}
+          data={`${supply.amount}`}
+          statsData={blockchainStats.supply}
         />
       </Styled.StatsCardsDeck>
-      <Styled.BlockSearch
-        size="large"
-        placeholder={counterpart.translate(
-          `pages.blocks.blockchain.search_blocks`
-        )}
-        onSearch={handleSearch}
-        loading={loading}
-      />
-      <div>
-        <h3>
+      <Styled.BlockHeaderBar>
+        <Styled.BlockHeader>
           {counterpart.translate(`pages.blocks.blockchain.recent_blocks`)}
-        </h3>
-      </div>
-      <BlockTable
-        blocks={
-          searchValue && searchValue !== ""
-            ? searchResult
-            : blockchainData.recentBlocks
-        }
-        loading={loading}
-      />
+          <InfoCircleOutlined />
+        </Styled.BlockHeader>
+        <SearchTableInput
+          columns={blockColumns}
+          dataSource={blockchainTableRows}
+          setDataSource={setSearchDataSource}
+          inputProps={{
+            placeholder: counterpart.translate(
+              `pages.blocks.blockchain.search_blocks`
+            ),
+            suffix: <SearchOutlined />,
+          }}
+        />
+        <Styled.DownloadLinks>
+          <DownloadOutlined />
+          <ReactToPrint
+            trigger={() => <a href="#">{counterpart.translate(`links.pdf`)}</a>}
+            content={() => componentRef.current}
+          />
+
+          {` / `}
+          <CSVLink
+            filename={"BlocksTable.csv"}
+            data={blockchainTableRows}
+            className="btn btn-primary"
+          >
+            {counterpart.translate(`links.csv`)}
+          </CSVLink>
+        </Styled.DownloadLinks>
+      </Styled.BlockHeaderBar>
+      {sm ? (
+        <List
+          itemLayout="vertical"
+          dataSource={searchDataSource}
+          loading={loading}
+          renderItem={(item) => (
+            <Link href={`/blockchain/${item.blockID}`} passHref>
+              <Styled.BlockListItem key={item.key}>
+                <Styled.BlockItemContent>
+                  <div className="item-info">
+                    <span className="item-info-title">
+                      {blockColumns[0].title()}
+                    </span>
+                    <span className="item-info-value">{item.blockID}</span>
+                  </div>
+                  <div className="item-info">
+                    <span className="item-info-title">
+                      {blockColumns[1].title()}
+                    </span>
+                    <span className="item-info-value">{item.time}</span>
+                  </div>
+                  <div className="item-info">
+                    <span className="item-info-title">
+                      {blockColumns[2].title()}
+                    </span>
+                    <span className="item-info-value">{item.witness}</span>
+                  </div>
+                  <div className="item-info">
+                    <span className="item-info-title">
+                      {blockColumns[3].title()}
+                    </span>
+                    <span className="item-info-value">{item.transaction}</span>
+                  </div>
+                </Styled.BlockItemContent>
+              </Styled.BlockListItem>
+            </Link>
+          )}
+        />
+      ) : (
+        <Styled.BlockTable
+          dataSource={searchDataSource}
+          columns={blockColumns}
+          // rowKey={(record) => record.blockID}
+          loading={loading}
+          pagination={
+            !loading
+              ? {
+                  showSizeChanger: false,
+                  size: "small",
+                  pageSize: 15,
+                  showLessItems: true,
+                  itemRender: (
+                    _page: number,
+                    type: "page" | "prev" | "next" | "jump-prev" | "jump-next",
+                    element: ReactNode
+                  ) => {
+                    if (type === "prev") {
+                      return (
+                        <a style={{ marginRight: "8px" } as CSSProperties}>
+                          {counterpart.translate(`buttons.previous`)}
+                        </a>
+                      );
+                    }
+                    if (type === "next") {
+                      return (
+                        <a style={{ marginLeft: "8px" } as CSSProperties}>
+                          {counterpart.translate(`buttons.next`)}
+                        </a>
+                      );
+                    }
+                    return element;
+                  },
+                }
+              : false
+          }
+          onRow={(record, _rowIndex) => {
+            return {
+              onClick: (_event) => {
+                router.push(`/blockchain/${record.blockID}`);
+              },
+            };
+          }}
+        />
+      )}
+      <Styled.PrintTable>
+        <BlockPrintTable ref={componentRef} />
+      </Styled.PrintTable>
     </Styled.BlockTabWrapper>
   );
 };
