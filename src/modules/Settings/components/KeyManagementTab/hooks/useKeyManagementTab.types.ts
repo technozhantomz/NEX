0bd7c5e9e06df3be06ff842a7ef094b511f53631
Evan Lynch <elynch@peerplays.com>
2022-09-23 11:17:23 +0000

    Resolve "NEX-FS08-23-27 | Settings page- Key mangement tab | Key Management Setting"

--- src/modules/Settings/components/KeyManagementTab/hooks/useKeyManagementTab.types.ts
+++ src/modules/Settings/components/KeyManagementTab/hooks/useKeyManagementTab.types.ts
@@ -1,9 +1,10 @@
-import { GeneratedKey } from "../../../../../common/types";
+import { GeneratedKey, PublicKeys } from "../../../../../common/types";
 import { CheckboxValueType, FormInstance, Rule } from "../../../../../ui/src";
 
 export type UseKeyManagementTabResult = {
   keyManagementForm: FormInstance<KeyManagementForm>;
   formValidation: FormValidation;
+  publicKeys: PublicKeys[];
   generatedKeys: GeneratedKey[];
   handleCheckboxChange: (checkedValues: CheckboxValueType[]) => void;
   selectedKeys: CheckboxValueType[];
