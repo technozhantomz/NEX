cca9900ec9ce03b50fd1d38609671cd4a34b8755
alimirpp <amirkharaz@peerplays.com>
2022-09-23 14:53:24 +0330

    #353 removing old folders

--- src/common/hooks/connectionManager/useConnectionManager.types.ts
+++
@@ -1,9 +0,0 @@
-export type UseConnectionManagerResult = {
-  apiConnected: boolean;
-  apiError: boolean;
-  syncError: boolean | null;
-  status: string;
-  willTransitionTo: (appInit?: boolean | undefined) => Promise<void>;
-  setSuccessConnectionStates: () => void;
-  setFailureConnectionStates: (e: any) => void;
-};
