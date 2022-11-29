892d68a40c901e765d0c9e3708316a7a29361bdc
0xPaperPlane <kalkheder@peerplays.com>
2022-11-28 22:50:27 -0800

    (#592) clean up comments

--- src/api/utils/__tests__/utils.test.js
+++ src/api/utils/__tests__/utils.test.js
@@ -3,40 +3,28 @@ import { isArrayEqual } from "../isArrayEqual";
 
 //Test: isArrayEqual
 //test1
-//input: two equal arrays
-//output (expected): true
 test("[3,2,1] is equal to [5,1] => 3+2+1=6 == 5+1=6", () => {
   expect(isArrayEqual([3, 2, 1], [3, 2, 1])).toBe(true);
 });
 //test2
-//input: two equal arrays (different order)
-//output (expected): true
 test("[3,2,1] is equal to [1,2,3] => 3 + 2 + 1 = 6 == 1+2+3 = 6", () => {
   expect(isArrayEqual([3, 2, 1], [1, 2, 3])).toBe(true);
 });
 //test3
-//input: two unequal arrays
-//output (expected): true
 test("[3,2,1] is not equal to [5,1,4] => 3 + 2 + 1 = 6 == 5+1+4 = 10", () => {
   expect(isArrayEqual([3, 2, 1], [5, 1, 4])).toBe(false);
 });
 //test4
-//input: two empty arrays
-//output (expected): true
 test("[] is equal to []", () => {
   expect(isArrayEqual([], [])).toBe(true);
 });
 //test5
-//input: two null arrays
-//output (expected): true
 test("null is equal to null", () => {
   expect(isArrayEqual(null, null)).toBe(true);
 });
 //test6
-//input: two undefined arrays
-//output (expected): true
-test("null is equal to null", () => {
-  expect(isArrayEqual(null, null)).toBe(true);
+test("undefined is equal to undefined", () => {
+  expect(isArrayEqual(undefined, undefined)).toBe(true);
 });
 
 //Test: utils.isNumber
@@ -44,11 +32,11 @@ test("null is equal to null", () => {
 test('"block" writes to => "block"', () => {
   expect(utils.isNumber("1")).toBe(true);
 });
-//test1
+//test2
 test('"one" => false', () => {
   expect(utils.isNumber("one")).toBe(false);
 });
-//test1
+//test3
 test("1 => true", () => {
   expect(utils.isNumber(1)).toBe(true);
 });
@@ -92,12 +80,6 @@ test('the blockchain for the symbol "BTC" is Bitcoin', () => {
 test('the blockchain for the symbol "btc" is Bitcoin', () => {
   expect(utils.getBlockchainFromSymbol("btc")).toBe("Bitcoin");
 });
-
-//Test: utils.ensureInputNumberValidity
-//test1: how to provide event
-// test('1 is not a valid number', () => {
-//     expect(utils.ensureInputNumberValidity(1)).toBe(false);
-// });
 
 //Test: utils.trimNum
 //test1
