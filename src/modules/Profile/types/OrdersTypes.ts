2e68c7d802caaf825ec911dcfa2140d300b6ee12
Ali Mir <amirkharaz@peerplays.com>
2022-12-06 16:19:41 +0000

    Feature/342 nex fs06 18 23 open order tab

---
+++ src/modules/Profile/types/OrdersTypes.ts
@@ -0,0 +1,12 @@
+export type OrderTableRow = {
+  key: string;
+  date: string;
+  pair: string;
+  type: string;
+  side: string;
+  price: string;
+  amount: string;
+  filled: string;
+  total: string;
+  statusActions?: string;
+};
