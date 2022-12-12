3159cfb22a07c3f9312fd7907b8c8d9670b59790
blockchain jsdev <7026221-blockchain-jsdev@users.noreply.gitlab.com>
2022-12-12 12:50:01 +0000

    unit test for #591 and #592 params and utils

--- jest.config.js
+++ jest.config.js
@@ -11,7 +11,7 @@ const createJestConfig = nextJest({
 /** @type {import('jest').Config} */
 const customJestConfig = {
   // Add more setup options before each test is run
-  setupFilesAfterEnv: ["./jest.setup.ts"],
+  setupFilesAfterEnv: ["./jest.setup.ts", "jest-extended/all"],
   // if using TypeScript with a baseUrl set to the root directory then you need the below for alias' to work
   moduleDirectories: ["node_modules", "<rootDir>/"],
   testEnvironment: "jest-environment-jsdom",
--- jest.setup.ts
+++ jest.setup.ts
@@ -1,11 +1,7 @@
-// Optional: configure or set up a testing framework before each test.
-// If you delete this file, remove `setupFilesAfterEnv` from `jest.config.js`
-
-// Used for __tests__/testing-library.js
-// Learn more: https://github.com/testing-library/jest-dom
-import "@testing-library/jest-dom/extend-expect";
 import { TextDecoder, TextEncoder } from "util";
 
+import { loadEnvConfig } from "@next/env";
+import "@testing-library/jest-dom/extend-expect";
 import counterpart from "counterpart";
 
 import { defaultLocales } from "./src/api/params";
@@ -18,3 +14,6 @@ defaultLocales.forEach(({ type, json }) =>
 // Text encoder
 (global as any).TextEncoder = TextEncoder;
 (global as any).TextDecoder = TextDecoder;
+
+// local env
+loadEnvConfig(process.cwd());
