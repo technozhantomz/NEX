bf05710f627b696b1e4a516c7c1d4a47183da2fe
alimirpp <amirkharaz@peerplays.com>
2022-12-13 17:18:20 +0330

    formating

--- src/modules/Profile/components/OrdersTable/OrdersTable.styled.tsx
+++ src/modules/Profile/components/OrdersTable/OrdersTable.styled.tsx
@@ -36,7 +36,9 @@ export const OrdersTable = styled(UiTable)`
     color: ${colors.textColorSecondary};
     font-weight: 300;
     border: none;
-    &:not(:last-child):not(.ant-table-selection-column):not(.ant-table-row-expand-icon-cell):not([colspan])::before {
+    &:not(:last-child):not(.ant-table-selection-column):not(
+        .ant-table-row-expand-icon-cell
+      ):not([colspan])::before {
       display: none;
     }
   }
