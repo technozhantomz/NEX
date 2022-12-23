831fe33fba3a0323d4ef0875fb5aa5569f0289bf
blockchain-jsdev <>
2022-12-23 16:08:57 +0530

    #611 fixing issues from previous tests

--- src/modules/Market/components/OrderBook/components/UserOrderColumns/__test__/UserOrderColumns.spec.tsx
+++ src/modules/Market/components/OrderBook/components/UserOrderColumns/__test__/UserOrderColumns.spec.tsx
@@ -13,12 +13,12 @@ describe("UserOrderColumns", () => {
     expect(userOrderColumns).toHaveLength(6);
   });
 
-  // it("should return a column with a cancel icon that calls the handleClick function when clicked", () => {
-  //   const cancelColumn = userOrderColumns[4];
-  //   const mockEvent = { stopPropagation: jest.fn(), preventDefault: jest.fn() };
-  //   cancelColumn.render("value", { key: "123" }).props.onClick(mockEvent);
-  //   expect(mockEvent.stopPropagation).toHaveBeenCalled();
-  //   expect(mockEvent.preventDefault).toHaveBeenCalled();
-  //   expect(handleClick).toHaveBeenCalledWith("123");
-  // });
+  it("should return a column with a cancel icon that calls the handleClick function when clicked", () => {
+    const cancelColumn = userOrderColumns[5];
+    const mockEvent = { stopPropagation: jest.fn(), preventDefault: jest.fn() };
+    cancelColumn.render("value", { key: "123" }).props.onClick(mockEvent);
+    expect(mockEvent.stopPropagation).toHaveBeenCalled();
+    expect(mockEvent.preventDefault).toHaveBeenCalled();
+    expect(handleClick).toHaveBeenCalledWith("123");
+  });
 });
