418d09dcac11214793d534edbf4f9fea5279900a
blockchain jsdev <7026221-blockchain-jsdev@users.noreply.gitlab.com>
2023-01-06 12:42:29 +0000

    #586 minnor fixes on transaction page

--- src/modules/Blockchain/components/TransactionDetails/components/OperationsTable/OperationsColumns.tsx
+++ src/modules/Blockchain/components/TransactionDetails/components/OperationsTable/OperationsColumns.tsx
@@ -4,9 +4,10 @@ import { TableHeading } from "../../../../../../common/components";
 
 import * as Styled from "./OperationsTable.styled";
 
-const headings = ["number", "operation_id", "operation_type", "fees", ""];
-const keys = ["number", "id", "type", "fees", "details"];
+const headings = ["number", "operation_id", "type", "time", "fees", ""];
+const keys = ["number", "id", "type", "time", "fees", "details"];
 const renders = [
+  undefined,
   undefined,
   undefined,
   (type: string): JSX.Element => <Styled.TimeStamp>{type}</Styled.TimeStamp>,
@@ -17,15 +18,44 @@ const renders = [
     </a>
   ),
 ];
-const filters = [undefined, undefined, undefined, undefined, undefined];
-const filterModes = [undefined, undefined, undefined, undefined, undefined];
-const filterSearch = [undefined, undefined, undefined, undefined, undefined];
-const onFilters = [undefined, undefined, undefined, undefined, undefined];
+const filters = [
+  undefined,
+  undefined,
+  undefined,
+  undefined,
+  undefined,
+  undefined,
+];
+const filterModes = [
+  undefined,
+  undefined,
+  undefined,
+  undefined,
+  undefined,
+  undefined,
+];
+const filterSearch = [
+  undefined,
+  undefined,
+  undefined,
+  undefined,
+  undefined,
+  undefined,
+];
+const onFilters = [
+  undefined,
+  undefined,
+  undefined,
+  undefined,
+  undefined,
+  undefined,
+];
 const sorters = [
   (a: { number: number }, b: { number: number }) => a.number - b.number,
   (a: { id: number }, b: { id: number }) => a.id - b.id,
-  (a: { type: string }, b: { type: string }) =>
-    new Date(a.type).getTime() - new Date(b.type).getTime(),
+  undefined,
+  (a: { time: string }, b: { time: string }) =>
+    new Date(a.time).getTime() - new Date(b.time).getTime(),
   (a: { fees: number }, b: { fees: number }) => a.fees - b.fees,
   undefined,
 ];
