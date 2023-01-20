778d02a55451b75afe143afe8617e1b6828239c5
alimirpp <amirkharaz@peerplays.com>
2023-01-20 18:37:11 +0330

    #619 fixing responsive issue in transaction and operations page

--- src/modules/Blockchain/components/TransactionDetails/TransactionDetails.styled.tsx
+++ src/modules/Blockchain/components/TransactionDetails/TransactionDetails.styled.tsx
@@ -38,7 +38,7 @@ export const BlockCard = styled(UiCard)`
 export const StatsCardsDeck = styled(UIStatsCardsDeck)``;
 
 export const BlockWrapper = styled.div`
-  margin: 35px;
+  margin: 20px 15px;
   max-width: 800px;
 `;
 
