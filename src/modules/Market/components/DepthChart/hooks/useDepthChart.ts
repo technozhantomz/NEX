d00c6d186ea31cce183dca41c10f605061097438
alimirpp <amirkharaz@peerplays.com>
2023-02-20 13:15:48 +0330

    #642 Fixing sonar issues

--- src/modules/Market/components/DepthChart/hooks/useDepthChart.ts
+++ src/modules/Market/components/DepthChart/hooks/useDepthChart.ts
@@ -3,6 +3,7 @@ import { cloneDeep } from "lodash";
 import { useCallback, useEffect, useState } from "react";
 
 import { useMarketContext } from "../../../../../common/providers";
+import { MarketOrder } from "../../../../../common/types";
 import { options } from "../DepthChartOptions";
 
 import {
@@ -23,29 +24,29 @@ export function useDepthChart(): UseDepthChartResult {
   >(depthChartOptions.series as Highcharts.SeriesAreaOptions[]);
   const { asks, bids } = useMarketContext();
 
+  const sortOrders = useCallback((a: MarketOrder, b: MarketOrder) => {
+    if (Number(a.quote) - Number(b.quote) !== 0) {
+      return Number(a.quote) - Number(b.quote);
+    }
+    return Number(a.base) - Number(b.base);
+  }, []);
+
   const formDepthChartData = useCallback(() => {
     //sort quote and base
     if (!asks || !bids) return;
     const clonedAsks = cloneDeep(asks);
     const clonedBids = cloneDeep(bids);
-    const sortedAsks = clonedAsks
-      .sort((a, b) => {
-        if (Number(a.quote) - Number(b.quote) !== 0) {
-          return Number(a.quote) - Number(b.quote);
-        }
-        return Number(a.base) - Number(b.base);
-      })
-      .map((row) => [Number(row.quote), Number(row.base)] as DepthData);
-    const sortedBids = clonedBids
-      .sort((a, b) => {
-        if (Number(a.quote) - Number(b.quote) !== 0) {
-          return Number(a.quote) - Number(b.quote);
-        }
-        return Number(a.base) - Number(b.base);
-      })
-      .map((row) => [Number(row.quote), Number(row.base)] as DepthData);
-    setDepthChartData({ asks: sortedAsks, bids: sortedBids });
-  }, [asks, bids]);
+    clonedAsks.sort(sortOrders);
+    clonedBids.sort(sortOrders);
+    const depthChartAsks = clonedAsks.map(
+      (row) => [Number(row.quote), Number(row.base)] as DepthData
+    );
+    const depthChartBids = clonedBids.map(
+      (row) => [Number(row.quote), Number(row.base)] as DepthData
+    );
+
+    setDepthChartData({ asks: depthChartAsks, bids: depthChartBids });
+  }, [asks, bids, sortOrders]);
 
   useEffect(() => {
     formDepthChartData();
